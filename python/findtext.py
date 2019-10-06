05f5dfcbcb4678de0dbefc9f9e8f511d41781f9b
SuhyeongCho <ch5951130@naver.com>
2019-10-06 10:24:17 +0900

    주석 +

--- python/findtext.py
+++ python/findtext.py
@@ -5,6 +5,9 @@ import cv2
 
 
 def findtext(imgcolorc, imgcolorf):
+    #imgcolorc : 원본이미지를 도형에 맞게 컷팅해준 사진이다.
+    #imgcolorf : imgcolorc사진의 글자 부분을 지운 사진이다.
+    
     img = imgcolorc
     img2 = imgcolorf
     row, col, _ = img.shape
@@ -13,50 +16,60 @@ def findtext(imgcolorc, imgcolorf):
     distcol = (int)(col * 0.2)
     distrow2 = (int)(row2 * 0.2)
     distcol2 = (int)(col2 * 0.2)
-    img = img[0 + distrow:row - distrow, 0 + distcol:col - distcol]
-    img2 = img2[0 + distrow2:row2 - distrow2, 0 + distcol2:col2 - distcol2]
+    img = img[0 + distrow:row - distrow, 0 + distcol:col - distcol] #이미지의 크기를 설정
+    img2 = img2[0 + distrow2:row2 - distrow2, 0 + distcol2:col2 - distcol2] #이미지의 크기를 설정
 
-    grayimage = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    grayimage = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)   #이미지를 GrayScale(0~255 까지 밝기의 농도를 가진 사진)로 변경한다.
     row, col, _ = img.shape
-        # time.sleep(1)
-    grayimage2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+    
+    grayimage2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY) #이미지를 GrayScale(0~255 까지 밝기의 농도를 가진 사진)로 변경한다.
 
-    sobelX = cv2.Sobel(grayimage, cv2.CV_64F, 1, 0)
-    sobelY = cv2.Sobel(grayimage, cv2.CV_64F, 0, 1)
+    sobelX = cv2.Sobel(grayimage, cv2.CV_64F, 1, 0) #grayimage의 경계를 흐리게 하기위해 Sobel X Grediant를 한다.
+    sobelY = cv2.Sobel(grayimage, cv2.CV_64F, 0, 1) #grayimage의 경계를 흐리게 하기위해 Sobel Y Grediant를 한다.
 
-    sobelX2 = cv2.Sobel(grayimage2, cv2.CV_64F, 1, 0)
-    sobelY2 = cv2.Sobel(grayimage2, cv2.CV_64F, 0, 1)
+    sobelX2 = cv2.Sobel(grayimage2, cv2.CV_64F, 1, 0)   #grayimage2의 경계를 흐리게 하기위해 Sobel X Grediant를 한다.
+    sobelY2 = cv2.Sobel(grayimage2, cv2.CV_64F, 0, 1)   #grayimage2의 경계를 흐리게 하기위해 Sobel Y Grediant를 한다.
 
-    sobelX = np.uint8(np.absolute(sobelX))
-    sobelY = np.uint8(np.absolute(sobelY))
+    sobelX = np.uint8(np.absolute(sobelX))  #Grediant 한 값을 부호없는 정수형으로 바꾼다.
+    sobelY = np.uint8(np.absolute(sobelY))  #Grediant 한 값을 부호없는 정수형으로 바꾼다.
 
-    sobelX2 = np.uint8(np.absolute(sobelX2))
-    sobelY2 = np.uint8(np.absolute(sobelY2))
+    sobelX2 = np.uint8(np.absolute(sobelX2))    #Grediant 한 값을 부호없는 정수형으로 바꾼다.
+    sobelY2 = np.uint8(np.absolute(sobelY2))    #Grediant 한 값을 부호없는 정수형으로 바꾼다.
 
-    grayimage = sobelX + sobelY
-    grayimage2 = sobelX2 + sobelY2
+    grayimage = sobelX + sobelY #각각 X,Y로 Grediant 한 값을 더해준다.
+    grayimage2 = sobelX2 + sobelY2  #각각 X,Y로 Grediant 한 값을 더해준다.
 
+    # image2에서 0이지만 image1에서 0이 아닌 부분을 0으로 바꾸어 경계를 흐리게해준다.
     row, col = grayimage.shape
     for i in range(0, row):
         for j in range(0,col) :
             if grayimage2[i][j] == 0 and grayimage[i][j] !=0:
                 grayimage[i][j] == 0
+
+    # image1에서 image2값을 빼주고 만약 그 값이 음수 라면 부호없는 정수이기 때문에 0으로 바꿔준다.
     for i in range(0, row):
         for j in range(0, col):
             if grayimage[i][j] >= grayimage[i][j] - grayimage2[i][j]:
                 grayimage[i][j] = grayimage[i][j] - grayimage2[i][j]
+
+    # 0와 255의 구분을 명확히 하기위해 threshold의 임계값을 계산해준다.
     sum = 0
     for i in range(0, row):
         for j in range(0, col):
             sum = sum + grayimage[i][j]
-
     sum = (int)(sum / (row * col))
     sum = sum + (int)(sum * 0.1)
 
+    # 알약 중심에 글자가 있기 때문에 GaussianBlur를 통해 중심값을 높여준다.
     grayimage = cv2.GaussianBlur(grayimage, (3, 3), 0)
+
+    # contours를위해 0와 255의 구분을 명확히 하기위해 threshold를 해준다.
     _, th = cv2.threshold(grayimage, sum, 255, 0)
+
+    #findcontours를 이용하여 사진에서 글자별로 박스를 추출한다.
     contours, _ = cv2.findContours(th, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
 
+    #BoundingRect의 가장 큰 값을 구한다.
     max = 0
     for i in range(1, len(contours)):
         cnt = contours[i]
@@ -66,6 +79,8 @@ def findtext(imgcolorc, imgcolorf):
     textarr = []
     origarr = []
     xarr = []
+
+    #contours만큼 반복문을 돌며 글자를 textarr에 input한다.
     for i in range(0, len(contours)):
         cnt = contours[i]
         x, y, w, h = cv2.boundingRect(cnt)
@@ -78,6 +93,7 @@ def findtext(imgcolorc, imgcolorf):
         origarr.append(tmp.copy())
         xarr.append(x)
 
+    # x의 순서순으로 배열을 정렬해준다.
     xlength = len(xarr)
     for i in range(0, xlength):
         for j in range(0, xlength):
@@ -93,4 +109,5 @@ def findtext(imgcolorc, imgcolorf):
                 tmp = textarr[i].copy()
                 textarr[i] = textarr[j].copy()
                 textarr[j] = tmp.copy()
+    #전처리된 알약의 문자를 하나하나 저장해 배열로 return 한다.
     return origarr, textarr
--- python/image_processing.py
+++ python/image_processing.py
@@ -3,7 +3,7 @@
 import cv2
 import numpy as np
 
-
+ #이미지에서 배경부분을 지우기 위한 함수
 def backproject(source, target, levels=2, scale=1):
     hsv = cv2.cvtColor(source, cv2.COLOR_BGR2HSV)
     hsvt = cv2.cvtColor(target, cv2.COLOR_BGR2HSV)
@@ -15,8 +15,8 @@ def backproject(source, target, levels=2, scale=1):
     dst = cv2.calcBackProject([hsvt], [0, 1], roihist, [0, 180, 0, 256], scale)
     return dst
 
-
-def text(img):
+#알약의 글씨를 지우는 함수
+def text(img):              
     imgx = img
 
     imglast = imgx.copy()
@@ -28,26 +28,35 @@ def text(img):
     cntb = 0
     cntf = 0
 
-    #   kernel size 3으로 설정
+    #kernel size 3으로 설정
 
     kernel = np.ones((3, 3), np.uint8)
     imgx1 = cv2.morphologyEx(imgx, cv2.MORPH_CLOSE, kernel)
 
-    #   kernel size 증가하면서 전체 크기 변할시 그 전 값 리턴
+    #kernel size 증가하면서 전체 크기 변할시 그 전 값 리턴
 
     for ksize in range(3, row, 2):
         kernel = np.ones((ksize, ksize), np.uint8)
-        imgx2 = cv2.morphologyEx(imgx, cv2.MORPH_CLOSE, kernel)
 
+
+        #kernel을 이동해가며 kernel size 안의 픽셀을 검사해 더 많은 픽셀의 bgr값으로 나머지 픽셀을 수정
+        #약의 글씨부분을 약의 색깔과 비슷하게 만듬
+
+        imgx2 = cv2.morphologyEx(imgx, cv2.MORPH_CLOSE, kernel)     
         b, g, r = cv2.split(imgx2)
 
+        #morphologyEx함수를 적용한 이미지의 알약부분 픽셀수를 셈
         for i in range(int(row / 2)):
             for j in range(int(col / 2)):
-                if b[i][j] + g[i][j] + r[i][j] > 30:
+                if b[i][j] + g[i][j] + r[i][j] > 30:    
                     cntf += 1
+
+        #이전의 이미지의 픽셀수와 비교하여 알약의 이미지가 원본보다 커졌을때
+        #글씨가 충분히 지워졌을거라 판단하고 리턴
+
         if cntb is 0:
-            cntb = cntf
-        elif cntf - cntb > int(cntb * 0.02):
+            cntb = cntf                                 
+        elif cntf - cntb > int(cntb * 0.02):            
             break
         imgx1 = imgx2
         cntf = 0
@@ -55,20 +64,26 @@ def text(img):
     return imgx1
 
 
-def fill(imgx, imglast, threshMap, img_size):
+def fill(imgx, imglast, threshMap, img_size): #이미지에서 알약부분의 빈곳을 채우는 함수
     row, col = img_size
     kernel = np.ones((3, 3), np.uint8)
-    threshMap1 = cv2.morphologyEx(threshMap, cv2.MORPH_CLOSE, kernel)
 
-    for i1 in range(row):
+
+    #kernel을 이동해가며 kernel size 안의 픽셀을 검사해 더 많은 픽셀의 bgr값으로 나머지 픽셀을 수정
+    #알약부분에 빈공간이 일부분 채워지게 된다
+    #이미지에서 알약부분의 중앙 최상단, 중앙 최좌단의 좌표를 i1과 j1에 저장한다.
+    threshMap1 = cv2.morphologyEx(threshMap, cv2.MORPH_CLOSE, kernel)   
+                                                                        
+    for i1 in range(row):                                               
         if threshMap1[i1][(int)(col / 2)] > 20:
             break
     for j1 in range(col):
         if threshMap1[(int)(row / 2)][j1] > 20:
             break
     size = 5
-    while (size < 20):
+    while (size < 20):       
 
+        #kenel size를 증가시키면서 morphologyEx함수를 적용시키고
         kernel = np.ones((size, size), np.uint8)
         threshMap2 = cv2.morphologyEx(threshMap, cv2.MORPH_CLOSE, kernel)
 
@@ -79,7 +94,8 @@ def fill(imgx, imglast, threshMap, img_size):
         for j2 in range(col):
             if threshMap2[(int)(row / 2)][j2] > 20:
                 break
-        if i1 != i2 or j1 != j2:
+        #이미지의 알약 크기가 변동하면 채우지 않아야 하는 부분을 채웠으므로 그전의 백업해두었던 이미지를 밑의 과정에서 사용한다.
+        if i1 != i2 or j1 != j2:                                    
             break
         else:
             threshMap1 = threshMap2
@@ -87,10 +103,12 @@ def fill(imgx, imglast, threshMap, img_size):
             j1 = j2
 
     threshMap = threshMap1
-    threshMapfilter = cv2.GaussianBlur(threshMap, (7, 7), 0)
+    #GaussianBlur를 적용
+    threshMapfilter = cv2.GaussianBlur(threshMap, (7, 7), 0)    
     imglast2 = imglast.copy()
+    #threshMap의 이미지에서 알약부분을 검출하였으므로 원본 이미지에 동일좌표를 적용하여 원본 이미지에서 알약부분을 검출한다.
     for i in range(row):
-        for j in range(col):
+        for j in range(col):                                    
             if (threshMap[i][j] == 0):
                 imgx[i][j] = 0
                 imglast[i][j] = 0
@@ -100,8 +118,8 @@ def fill(imgx, imglast, threshMap, img_size):
                 imglast2[i][j] = 0
     return imglast, imglast2, threshMap, threshMapfilter
 
-
-def rgb2hsv(r, g, b):
+#bgr채널의 이미지를 hsv채널로 변경하는 함수
+def rgb2hsv(r, g, b):                       
     r, g, b = r / 255.0, g / 255.0, b / 255.0
     mx = max(r, g, b)
     mn = min(r, g, b)
@@ -121,8 +139,8 @@ def rgb2hsv(r, g, b):
     v = mx
     return h, s, v * 100
 
-
-def color(bbb):
+#알약의 색을 검출하는 함수
+def color(bbb):                     
     b, g, r = cv2.split(bbb)
     cb = 0
     cg = 0
@@ -131,7 +149,8 @@ def color(bbb):
     h, w, _ = bbb.shape
     for i in range(h):
         for j in range(w):
-            if (b[i][j] != 0) and (g[i][j] != 0) and (r[i][j] != 0):
+            #알약부분의 픽셀들의 평균 bgr을 구한다.
+            if (b[i][j] != 0) and (g[i][j] != 0) and (r[i][j] != 0):            
                 cb += b[i][j]
                 cg += g[i][j]
                 cr += r[i][j]
@@ -139,18 +158,11 @@ def color(bbb):
     cb = (int)(cb / cnt)
     cg = (int)(cg / cnt)
     cr = (int)(cr / cnt)
-    #     for i in range(h):
-    #         for j in range(w):
-    #             if (b[i][j] < 20):
-    #                 b[i][j] = cb
-    #             if (g[i][j] < 20):
-    #                 g[i][j] = cg
-    #             if (r[i][j] < 20):
-    #                 r[i][j] = cr
-
-    h, _, v = rgb2hsv(cr, cg, cb)
-    # 검은색, 회색, 흰색 해야됨
-    if abs(cb - cg) + abs(cg - cr) + abs(cr - cb) < 50:
+  
+    #bgr 평균값을 hsv로 변환한다
+    #hsv 값을 이용하여 색을 판단
+    h, _, v = rgb2hsv(cr, cg, cb)                   
+    if abs(cb - cg) + abs(cg - cr) + abs(cr - cb) < 50:             
         if v < 30:
             color1 = 'black'
         elif v < 70:
@@ -174,18 +186,21 @@ def color(bbb):
 # 약 설정
 def image_processing(img):
     # 약 설정
-    # imgx = img[0:370, :]
     imgx = img
     imglast = imgx.copy()
-    cv2.pyrMeanShiftFiltering(imgx, 2, 10, imgx, 4)
+    #이미지의 노이즈를 pyrMeanShiftFiltering적용하여 제거한다
+    cv2.pyrMeanShiftFiltering(imgx, 2, 10, imgx, 4)         
 
-    backproj = np.uint8(backproject(imgx, imgx, levels=2))
+    #이미지에 배경을 제거하기 위한 객체를 생성.
+    backproj = np.uint8(backproject(imgx, imgx, levels=2))  
     backproj = 255 - backproj
 
 
-    cv2.normalize(backproj, backproj, 0, 255, cv2.NORM_MINMAX)
+    #히스토그램 균일화를 통해 배경을 제거
+    cv2.normalize(backproj, backproj, 0, 255, cv2.NORM_MINMAX) 
 
-    saliencies = [backproj, backproj, backproj]
+    #saliencyMap을 이용하여 특이점을 검출한다.
+    saliencies = [backproj, backproj, backproj]             
     saliency = cv2.merge(saliencies)
 
     cv2.pyrMeanShiftFiltering(saliency, 20, 200, saliency, 2)
@@ -193,7 +208,8 @@ def image_processing(img):
 
     saliencyMap = saliency
 
-    threshMap = cv2.threshold(saliencyMap.astype("uint8"), 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+    #threshold를 적용하여 특이점 이외의 부분을 제거
+    threshMap = cv2.threshold(saliencyMap.astype("uint8"), 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]      
 
     # hsv로 변환후 v가 0.5이하면 그림자로 취급 검은색으로 만듬
     hsv = cv2.cvtColor(imgx, cv2.COLOR_BGR2HSV)
@@ -224,37 +240,33 @@ def image_processing(img):
             x = (v[i][j] - min) / ((max - min) + 1e-10)
             if (x <= 0.3):
                 threshMap[i][j] = 0
-    # fill로 채우고 경계부분 검출
 
+    # fill로 채우고 경계부분 검출
     imglast, imglast2, threshMap, threshMapfilter = fill(imgx, imglast, threshMap, (row, col))
 
 
+    #알약의 경계를 findContours통해 찾음
+    contours, hierarchy = cv2.findContours(threshMapfilter * 1, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)     
 
-    # contours, hierarchy = cv2.findContours(threshMap * 1, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    #
-    # contours = sorted(contours, key=cv2.contourArea)
-
-   # x, y, w, h = cv2.boundingRect(contours[-1])
-
-   # imgcolorc = imglast[y:y + h, x:x + w]
-
-    contours, hierarchy = cv2.findContours(threshMapfilter * 1, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-
-    contours = sorted(contours, key=cv2.contourArea)
+    #이미지의 경계들의 좌표를 크기순으로 정렬
+    contours = sorted(contours, key=cv2.contourArea)                        
 
     # ------------------------------크기 정사각형 만드는 부분------------------------------------------
     flag = 0
     size = len(contours) -1
     for j in range(0,1000):
-        for i in range(-1, size):
+        for i in range(-1, size): 
+            #이미지에서 알약부분은 화면 중앙에 가깝게 위치해있으므로 위에서 찾은 경계들중 중앙에 가까운 경계를 찾음                                                      
             x, y, w, h = cv2.boundingRect(contours[i])
             if (x - j*10< 210 and x + w  + j*10> 210 and y - j*10< 280 and y + h + j*10> 280):
                 flag =1
                 break
         if flag == 1:
             break
-    imgcolorc = imglast[y:y + h, x:x + w]
-    if (h > w):
+    #찾은 경계부분을 이용 원본 이미지로부터 최종 알약부분을 검출
+    #찾은 알약의 이미지는 추후에 정사각형의 사이즈로 사용되기때문에 이미지를 정사각형으로 만들어줌
+    imgcolorc = imglast[y:y + h, x:x + w]               
+    if (h > w):                                         
         x = x - (int)((h - w) / 2)
         w = h
     else:
@@ -264,7 +276,11 @@ def image_processing(img):
 
     imgshape = threshMapfilter[y:y + h, x:x + w]
 
+    #알약의 색을 검출
     imgcolorc, colorname = color(imgcolorc)
-    imgcolorf = text(imgcolorc)
 
-    return imgcolorc, imgcolorf, imgshape, colorname
+    #글자검출에 필요한 이미지(글자부분을 지운 알약이미지)를 생성         
+    imgcolorf = text(imgcolorc)                     
+
+    #알약 이미지, 글자검출을 위한 이미지, 알약의 모양 이미지, 알약의 색 리턴
+    return imgcolorc, imgcolorf, imgshape, colorname    
